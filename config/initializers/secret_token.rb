a89617b4fd409d7ab73bfe7f4463e9f795e5ee31
Adel <adel.almaleh@gmail.com>
2016-03-10 15:47:43 +0800

    new blog created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdelBlog::Application.config.secret_key_base = 'f7cda0306b793157f65fa5610214475f928edd117a34f13fb22710159b50fbc83a169e7a9f5802f31065398426f6a7626d8810afdac4d5b24dc3b368e523bfee'
