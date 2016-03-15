5bb2ddf60583a2cd6f7678d2cf3793f8b0cc578a
Adel <adel.almaleh@gmail.com>
2016-03-15 16:00:56 +0800

    install bootstap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-	
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
 	def index
 		@articles= Article.all
 
@@ -9,11 +9,7 @@ class ArticlesController < ApplicationController
 		@article=Article.new
 	end
 
-
-
-
 	def edit
-		@article= Article.find(params[:id])
 	end
 
 
@@ -28,7 +24,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def update
-		@article= Article.find(params[:id]) 
 		if @article.update(article_params)
 		flash[:notice]= "Article was successfully updated"
 		redirect_to article_path(@article)
@@ -38,11 +33,10 @@ class ArticlesController < ApplicationController
 	end	
 
 	def show
-		@article = Article.find(params[:id])
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
+		
 		flash[:notice] = "article was successfully deleted"
 		redirect_to article_path
 	end
@@ -50,6 +44,10 @@ class ArticlesController < ApplicationController
 
 
 	private 
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
 		def article_params
 		params.require(:article).permit(:title, :description)
 	end
