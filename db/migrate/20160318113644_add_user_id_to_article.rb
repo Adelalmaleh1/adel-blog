d34d2ebdbb174e3a135d30ec27dd1af5003459e8
Adel Almaleh <adel.almaleh@gmail.com>
2016-04-03 17:15:15 +0800

    adding mysql2

--- db/migrate/20160318113644_add_user_id_to_article.rb
+++ db/migrate/20160318113644_add_user_id_to_article.rb
@@ -1,4 +1,5 @@
 class AddUserIdToArticle < ActiveRecord::Migration
   def change
   	add_column :articles, :user_id, :integer
+  end
 end
--- db/migrate/20160324093629_rename_created_at_column.rb
+++
@@ -1,7 +0,0 @@
-class RenameCreatedAtColumn < ActiveRecord::Migration
-
-  def change
-rename_column :articles, :created_add, :created_at
-rename_column :articles, :updated_add, :updated_at
-  end
-end
